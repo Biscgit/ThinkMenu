1f85fd8d2f639827ac615af3519480d806e33bd5
David Horvát <horvatda@proton.me>
2023-11-25 16:08:50 +0100

    created a temporary help menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process::ExitCode;
+
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1).map(String::as_str) {
+        // help menu
+        Some("-h" | "--help") => println!("Help menu!"),
+        Some(_) => println!("Unknown argument, use `--help` to see the help page"),
+
+        // normal program run
+        None => {
+
+        }
+    }
+
+    return ExitCode::from(0);
 }
