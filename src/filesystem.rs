e6ea9f23e107be057a162f1334c1f0afcb949da6
David Horvát <horvatda@proton.me>
2023-11-26 10:16:32 +0100

    methods to save and load values

---
+++ src/filesystem.rs
@@ -0,0 +1,18 @@
+use std::{io, fs};
+use std::io::{Read, Write};
+
+
+pub fn read_file(file_path: &str) -> io::Result<String> {
+    let contents = fs::read_to_string(file_path)?;
+
+    Ok(contents)
+}
+
+pub fn write_file(file_path: &str, content: &str) -> io::Result<()> {
+    let mut file = fs::OpenOptions::new().truncate(true).write(true).open(file_path)?;
+
+    file.write_all(content.as_bytes())?;
+    file.flush()?;
+
+    Ok(())
+}
