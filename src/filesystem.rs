ca8b106591dd143f075830b4b160ba01dea84542
David Horvát <horvatda@proton.me>
2023-11-26 19:01:17 +0100

    rewrote functions with tokio

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,18 +1,15 @@
-use std::{io, fs};
-use std::io::Write;
+use tokio::{io, fs};
+use tokio::io::AsyncWriteExt;
 
-
-pub fn read_file(file_path: &str) -> io::Result<String> {
-    let contents = fs::read_to_string(file_path)?;
-
+pub async fn read_file(file_path: &str) -> io::Result<String> {
+    let contents = fs::read_to_string(file_path).await?;
     Ok(contents)
 }
 
-pub fn write_file(file_path: &str, content: &str) -> io::Result<()> {
-    let mut file = fs::OpenOptions::new().truncate(true).write(true).open(file_path)?;
+pub async fn write_file(file_path: &str, contents: &str) -> io::Result<()> {
+    let mut file = fs::OpenOptions::new().truncate(true).write(true).open(file_path).await?;
 
-    file.write_all(content.as_bytes())?;
-    file.flush()?;
-
+    file.write_all(contents.as_bytes()).await?;
+    file.flush().await?;
     Ok(())
 }
